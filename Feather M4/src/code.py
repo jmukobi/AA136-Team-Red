e88fdb90e7a8d3014102d1eed3ec205573110bf9
jmukobi <jmukobi@stanford.edu>
2023-04-25 18:07:37 -0700

    Initial code commit

---
+++ Feather M4/src/code.py
@@ -0,0 +1,24 @@
+# type: ignore
+#PDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""CircuitPython Essentials: PWM with Fixed Frequency example."""
+import time
+import board
+import pwmio
+
+# LED setup for most CircuitPython boards:
+pwm = pwmio.PWMOut(board.D12, frequency=5000, duty_cycle=0)
+# LED setup for QT Py M0:
+# led = pwmio.PWMOut(board.SCK, frequency=5000, duty_cycle=0)
+
+print("Running")
+while True:
+    gas = 0
+    for i in range(100):
+        gas += 1/100
+        val = int(gas*65535)
+        print(val)
+        pwm.duty_cycle = val
+        time.sleep(.1)
